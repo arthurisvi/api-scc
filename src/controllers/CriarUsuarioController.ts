cb74eb0de99a651c068095a0fb82c6135396b79f
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-22 23:07:54 -0300

    controller usuario

--- src/controllers/CriarUsuarioController.ts
+++ src/controllers/CriarUsuarioController.ts
@@ -6,6 +6,7 @@ class CriarUsuarioController{
     async handle(req: Request, res: Response){
         const { nome, email, senha, n_whatsapp, nome_time } = req.body
 
+        
         const criarUsuarioService = new CriarUsuarioService();
 
         const usuario = await criarUsuarioService.execute({ nome, email, senha, n_whatsapp, nome_time })
